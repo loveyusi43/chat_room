8bf2c3d24402e0e0d9993d1e83d81e4bdf6daad0
loveyusi <loveyusi43@gmail.com>
2024-09-18 23:08:01 +0800

    服务端能够监听

--- socket.cc
+++ socket.cc
@@ -10,8 +10,51 @@ Socket::Socket(int family, int type) {
     }
 }
 
-bool Socket::Bind(const std::string& host, int port) {
-    ;
+bool Socket::Bind(const std::string& host, unsigned short int port) {
+    struct sockaddr_in sockaddr;
+    std::memset(&sockaddr, 0, sizeof(sockaddr));
+    sockaddr.sin_family = AF_INET;
+
+    // ip地址和端口要经过转换
+    if (host.empty())
+        sockaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    else
+        sockaddr.sin_addr.s_addr = inet_addr(host.c_str());  // 需要包含的头文件<arpa/inet.h>
+
+    sockaddr.sin_port = htons(port);
+
+    if (::bind(sockfd_, (struct sockaddr*)&sockaddr, sizeof(sockaddr)) < 0) {
+        throw std::runtime_error(std::format("绑定失败, 错误码: {}, 错误信息: {}\n", errno, strerror(errno)));
+    }
+
+    ip_addr_ = host;
+    port_ = port;
+
+    return true;
 }
+
+bool Socket::Listen(int backlog) {
+    if (::listen(sockfd_, backlog) < 0) {
+        throw std::runtime_error(std::format("监听失败, 错误码: {}, 错误信息: {}\n"));
+    }
+
+    std::cout << std::format("Server is listening on {}:{}", ip_addr_, port_) << std::endl;
+    return true;
+}
+
+Socket Socket::Accept(void) {
+    struct sockaddr_in client_address;
+    socklen_t client_addrlen = sizeof(client_address);
+
+    int connfd = ::accept(sockfd_, (struct sockaddr*)&client_address, &client_addrlen);
+    if (connfd < 0) {
+        throw std::runtime_error(std::format("获取客户端连接失败, 错误码: {}, 错误信息: {}\n", errno, strerror(errno)));
+    }
+
+    char client_ip[INET_ADDRSTRLEN];  // IPv4地址长度
+    inet_ntop(AF_INET, &client_address.sin_addr, client_ip, INET_ADDRSTRLEN);
+    int client_port = ntohs(client_address.sin_port);
+    std::cout << "Client connected - IP: " << client_ip << ", Port: " << client_port << std::endl;
+    return Socket(connfd, client_ip, client_port);
 
 }  // namespace ljh::socket
--- socket.h
+++ socket.h
@@ -1,22 +1,33 @@
 #ifndef __SOCKET_H
 #define __SOCKET_H
 
-#include <sys/socket.h>
+#include <arpa/inet.h>
 #include <netinet/in.h>
+#include <sys/socket.h>
 #include <cstring>
 
-#include <stdexcept>
+
 #include <format>
+#include <stdexcept>
+#include <iostream>
 
 namespace ljh::socket {
 class Socket {
 public:
     Socket(int family, int type);
 
-    bool Bind(const std::string& host, int port);
+    Socket(int connfd, std::string ip, unsigned short port);
+
+    bool Bind(const std::string& host, unsigned short int port);
+
+    bool Listen(int backlog);
+
+    Socket Accept(void);
 
 private:
     int sockfd_;
+    std::string ip_addr_;
+    unsigned short int port_;
 };
 }  // namespace ljh::socket
 
